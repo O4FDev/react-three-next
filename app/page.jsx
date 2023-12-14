be82cd3fc8ba2ebf7addd226f0e7383c3c804ba0
O4FDev <luke.lucas@ou.ac.uk>
2023-12-14 22:37:01 +0000

    fix: mobile overflow

--- app/page.jsx
+++ app/page.jsx
@@ -54,7 +54,7 @@ export default function Page() {
           <View orbit className='relative h-full  sm:h-48 sm:w-full'>
             <Suspense fallback={null}>
               <Dog scale={2} position={[0, -1.6, 0]} rotation={[0.0, -0.3, 0]} />
-              <Common color={'lightpink'} />
+              <Common color={'transparent'} />
             </Suspense>
           </View>
         </div>
@@ -63,7 +63,7 @@ export default function Page() {
           <View orbit className='relative h-full animate-bounce sm:h-48 sm:w-full'>
             <Suspense fallback={null}>
               <Duck route='/blob' scale={2} position={[0, -1.6, 0]} />
-              <Common color={'lightblue'} />
+              <Common color={'transparent'} />
             </Suspense>
           </View>
         </div>
